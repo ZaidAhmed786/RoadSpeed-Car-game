780b0ec20d1e553573dcf2a37279341996def91e
Zaid Ahmed <66408178+ZaidAhmed786@users.noreply.github.com>
2022-08-23 15:22:39 -0700

    day 3

--- Main.js
+++ Main.js
@@ -2,9 +2,9 @@ const score = document.querySelector('.score');
 const startScreen = document.querySelector('.startScreen');
 const gameArea = document.querySelector('.gameArea');
 
-
+let player = { speeed : 5};
 const keys = {ArrowUp : false , ArrowDown : false , ArrowLeft : false , ArrowRight : false}
-const player = {};
+
 const keyDown = (e) =>{
     e.preventDefault();
     keys[e.key] = true
@@ -18,9 +18,18 @@ const keyUp = (e) =>{
 
 const gamePlay = ()=>{
     console.log('hey game start now');
+    let car = document.querySelector('.car')
+    let road = gameArea.getBoundingClientRect();
     if(player.start)
     {
+        if(keys.ArrowUp && player.y > (road.top + 70)) { player.y -= player.speeed }
+        if(keys.ArrowDown && player.y < (road.bottom - 50)) {player.y += player.speeed}
+        if(keys.ArrowLeft && player.x > 0) { player.x -= player.speeed }
+        if(keys.ArrowRight && player.x < (road.width - 50)) {player.x += player.speeed }
+        car.style.top = player.y + "px";
+        car.style.left = player.x + "px";
         window.requestAnimationFrame(gamePlay);
+        
     }
     
 }
@@ -31,10 +40,25 @@ const start = () =>{
     startScreen.classList.add('hide')
     window.requestAnimationFrame(gamePlay);
 
+   
+
+            for(let x=0;x<5;x++){
+                let roadLine = document.createElement('div');
+                roadLine.setAttribute('class' , 'roadLine');
+                roadLine.style.top = (x*150) + "px";
+                gameArea.appendChild(roadLine);
+                
+            }
+
     let car = document.createElement('div');
     car.setAttribute('class' , 'car');
     // car.innerHTML = 'car' ;
     gameArea.appendChild(car);
+
+    player.x = car.offsetLeft;
+    player.y = car.offsetTop;
+
+    console.log(car.offsetLeft)
 
 }
 
