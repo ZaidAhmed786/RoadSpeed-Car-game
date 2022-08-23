ac3b951d671d0d6b2ad58ce7ed192026c2afb542
Zaid Ahmed <66408178+ZaidAhmed786@users.noreply.github.com>
2022-08-23 12:58:04 -0700

    day 2 done

--- Main.js
+++ Main.js
@@ -1,4 +1,49 @@
-const score = document.querySelector('.startScreen');
+const score = document.querySelector('.score');
+const startScreen = document.querySelector('.startScreen');
+const gameArea = document.querySelector('.gameArea');
 
 
-console.log(score)
+const keys = {ArrowUp : false , ArrowDown : false , ArrowLeft : false , ArrowRight : false}
+const player = {};
+const keyDown = (e) =>{
+    e.preventDefault();
+    keys[e.key] = true
+    console.log(keys);
+}
+const keyUp = (e) =>{
+    e.preventDefault();
+    keys[e.key] = false
+    // console.log(e.key);
+}
+
+const gamePlay = ()=>{
+    console.log('hey game start now');
+    if(player.start)
+    {
+        window.requestAnimationFrame(gamePlay);
+    }
+    
+}
+
+const start = () =>{
+    player.start = true;
+    gameArea.classList.remove('hide')
+    startScreen.classList.add('hide')
+    window.requestAnimationFrame(gamePlay);
+
+    let car = document.createElement('div');
+    car.setAttribute('class' , 'car');
+    // car.innerHTML = 'car' ;
+    gameArea.appendChild(car);
+
+}
+
+
+
+startScreen.addEventListener('click' , start)
+document.addEventListener('keydown' , keyDown);
+document.addEventListener('keyup' , keyUp);
+
+
+
+// console.log(score)
